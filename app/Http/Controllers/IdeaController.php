bd98d277065713034eb9909707788bb6ac57574a
creedx <guilhermejeffofc@gmail.com>
2024-06-28 15:41:46 -0300

    created update idea

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -16,16 +16,33 @@ class IdeaController extends Controller
     public function store(){
 
         request()->validate([
-            'idea' => 'required|min:5|max:250',
+            'content' => 'required|min:5|max:250',
         ]);
 
         $idea = Idea::create(
             [
-                "content"=> request()->get('idea', ''),
+                "content"=> request()->get('content', ''),
             ]
         );
 
         return redirect()->route('home')->with('success','Idea created succesfully');
+    }
+    
+    public function edit(Idea $idea){
+        $editing = true;
+        return view('ideas.show', compact('idea', 'editing'));
+    }
+
+    public function update(Idea $idea){
+        request()->validate([
+            'content' => 'required|min:5|max:250',
+
+        ]);
+        
+        $idea->content = request()->get('content', '');
+        $idea->save();
+
+        return redirect()->route('home')->with('success','Idea updated succesfully');
     }
 
     public function destroy(Idea $idea){
--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,5 @@ Route::get('/', [DashboardController::class,'index'])->name('home');
 Route::get('/ideas/{idea}', [IdeaController::class,'show'])->name('idea.show');
 Route::post('/ideas', [IdeaController::class,'store'])->name('idea.create');
 Route::delete('/ideas/{idea}', [IdeaController::class,'destroy'])->name('idea.destroy');
+Route::get('/ideas/{idea}/edit', [IdeaController::class,'edit'])->name("idea.edit");
+Route::put("/ideas/{idea}", [IdeaController::class,"update"])->name('idea.update');
